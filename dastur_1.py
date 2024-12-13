bb37f50c45da9d30078a658af2c78c0bdfac96bb
Ikromalieva-Sabokhat <ikromalievasabokhat@gmail.com>
2024-12-13 02:41:52 -0800

    versiya 1

--- dastur_1.py
+++ dastur_1.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+def transpose_matrix(matrix):
+    transposed_matrix=[]
+    for i in range(len(matrix[0])): #3
+        new_line = []
+        for j in range(len(matrix)):#4
+            new_line.append(matrix[j][i])
+        transposed_matrix.append(new_line)
+    return transposed_matrix
